b9a1cf1549e1daec94873dad66cb60fe6ab98ae4
gimee <gimee@mail.ru>
2021-07-10 11:57:27 +0300

    Final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,26 +10,22 @@ public class Main {
         FrontSystem fs = new FrontSystem();
         BackSystem bs = new BackSystem();
         ExecutorService executorService = Executors.newFixedThreadPool(3);
+        ExecutorService executorClient = Executors.newFixedThreadPool(5);
+        ExecutorService executorTreatment = Executors.newFixedThreadPool(2);
         for (int i = 0; i < 3; i++)
             executorService.submit(new Warming(latch,bs));
         executorService.shutdown();
         latch.await();
 
-        new Client(1, fs, 5000, "CREDIT").start();
-        new Client(2, fs, 6000, "PAYMENT").start();
-        new Client(3, fs, 7000, "CREDIT").start();
-        new Client(4, fs, 500, "PAYMENT").start();
-        new Client(5, fs, 9000, "CREDIT").start();
-        new Client(1, fs, 5000, "CREDIT").join();
-        new Client(2, fs, 5000, "CREDIT").join();
-        new Client(3, fs, 5000, "CREDIT").join();
-        new Client(4, fs, 5000, "CREDIT").join();
-        new Client(5, fs, 5000, "CREDIT").join();
-
-        new Treatment(1, fs, bs).start();
-        new Treatment(2, fs, bs).start();
-        new Treatment(1, fs, bs).join();
-        new Treatment(2, fs, bs).join();
+        executorClient.submit(new Client(1, fs, 5000, "CREDIT"));
+        executorClient.submit(new Client(2, fs, 6000, "PAYMENT"));
+        executorClient.submit(new Client(3, fs, 7000, "CREDIT"));
+        executorClient.submit(new Client(4, fs, 500, "PAYMENT"));
+        executorClient.submit(new Client(5, fs, 9000, "CREDIT"));
+        executorClient.shutdown();
+        executorTreatment.submit(new Treatment(1, fs, bs));
+        executorTreatment.submit(new Treatment(2, fs, bs));
+        executorTreatment.shutdown();
 
     }
 }
